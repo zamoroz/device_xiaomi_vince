c08171c360565cb09def4632956952c1c4a8be08
owl <moras8465132@gmail.com>
2019-08-25 14:51:41 +0700

    vince: Re-enable some UX optimizations from stock
    * These were removed in 0570b23 and 3d4186f as I blamed them
      for UI performance degradation.
    * Further testing shows that they actually reduce jank in some cases.

--- prop.mk
+++ prop.mk
@@ -250,6 +250,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.qti.sys.fw.trim_empty_percent=100 \
 	ro.vendor.qti.sys.fw.trim_cache_percent=100 \
 	ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+	
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
